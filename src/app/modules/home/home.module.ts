81138423bb821001a6d6c3b51d37814f6d8d23fd
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-08 09:19:16 -0300

    feat: add home module with dashboard component and routing

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { RouterModule } from '@angular/router';
+import { HOME_ROUTES } from './home.routing';
+
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { CookieService } from 'ngx-cookie-service';
+
+@NgModule({
+  declarations: [
+    DashboardHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(HOME_ROUTES),
+    // PrimeNG
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    CardModule,
+    ToastModule
+  ],
+  providers: [MessageService, CookieService]
+})
+export class HomeModule { }
---
+++ src/app/modules/home/home.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { DashboardHomeComponent } from "./page/dashboard-home/dashboard-home.component";
+
+export const HOME_ROUTES: Routes = [
+  {
+    path: '',
+    component: DashboardHomeComponent
+  }
+];
