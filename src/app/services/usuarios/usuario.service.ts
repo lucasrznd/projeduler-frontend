6d87368727f7e6fdce24a54e4e376c83291fc794
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-08 11:51:21 -0300

    feat: add UsuarioService for user management with API integration

---
+++ src/app/services/usuarios/usuario.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsuarioResponse } from 'src/app/models/interfaces/usuarios/UsuarioResponse';
+import { environment } from 'src/environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private API_URL = `${environment.API_URL}/usuarios`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllUsuarios(): Observable<Array<UsuarioResponse>> {
+    return this.http.get<Array<UsuarioResponse>>(this.API_URL);
+  }
+}
