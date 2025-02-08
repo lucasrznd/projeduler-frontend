c21df19fd8c018d9700e4c219c99818277060f65
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-08 10:28:44 -0300

    feat: add toolbar navigation component and integrate Chart.js for dashboard visualization

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
 import { RouterModule } from '@angular/router';
 import { HOME_ROUTES } from './home.routing';
+import { CookieService } from 'ngx-cookie-service';
 
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
@@ -11,7 +12,8 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { CookieService } from 'ngx-cookie-service';
+import { ChartModule } from 'primeng/chart';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,10 @@ import { CookieService } from 'ngx-cookie-service';
     ButtonModule,
     ToolbarModule,
     CardModule,
-    ToastModule
+    ToastModule,
+    ChartModule,
+    // Shared
+    SharedModule
   ],
   providers: [MessageService, CookieService]
 })
---
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-toolbar-navigation',
+  templateUrl: './toolbar-navigation.component.html',
+  styleUrls: ['./toolbar-navigation.component.scss']
+})
+export class ToolbarNavigationComponent {
+
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  logout(): void {
+    this.cookieService.delete('accessToken');
+    // this.cookieService.delete('refreshToken');
+    void this.router.navigate(['/login']);
+  }
+
+}
