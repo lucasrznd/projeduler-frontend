1838ed74c81ebbd2c3f9bd9b69ef11e621c6c33c
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-11 09:30:26 -0300

    feat: add Portuguese translations for PrimeNG configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.primeNGConfig.ripple = true;
+    this.primeNGConfig.setTranslation({ apply: 'Aplicar', clear: 'Limpar' })
   }
 }
