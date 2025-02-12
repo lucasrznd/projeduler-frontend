5d23c2eab59f5c7a4f4df61f35ad80369763b85c
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-12 14:40:49 -0300

    feat(projetos-table): enhance event handling for project actions with specific event types

--- src/app/modules/projetos/components/projetos-table/projetos-table.component.ts
+++ src/app/modules/projetos/components/projetos-table/projetos-table.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Table } from 'primeng/table';
+import { ProjetoEvent } from 'src/app/models/enums/projetos/ProjetoEvent';
 
 import { ProjetoResponse } from 'src/app/models/interfaces/projetos/ProjetoResponse';
+import { EventAction } from 'src/app/models/interfaces/usuarios/event/EventAction';
 
 @Component({
   selector: 'app-projetos-table',
@@ -11,8 +13,11 @@ import { ProjetoResponse } from 'src/app/models/interfaces/projetos/ProjetoRespo
 })
 export class ProjetosTableComponent {
   @Input() public projetos: Array<ProjetoResponse> = [];
+  @Output() projetoEvent = new EventEmitter<EventAction>();
 
   public projetoSelected!: ProjetoResponse;
+  public addProjetoEvent = ProjetoEvent.ADD_PROJETO_EVENT;
+  public editProjetoEvent = ProjetoEvent.EDIT_PROJETO_EVENT;
 
   constructor() { }
 
@@ -39,8 +44,12 @@ export class ProjetosTableComponent {
     return priorityMap[prioridade.toUpperCase()] || 'info';
   }
 
-  handleProjetoEvent(): void {
-
+  handleProjetoEvent(action: string, id?: number): void {
+    if (action && action !== '') {
+      const projetoEventData = id && id !== undefined ? { action, id } : { action };
+      // Emitir valor do evento
+      this.projetoEvent.emit(projetoEventData);
+    }
   }
 
   handleDeleteProjeto(): void {
