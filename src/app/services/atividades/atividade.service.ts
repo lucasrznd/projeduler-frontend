59389de9a1cf021667f5aa96d9764f8bdb2114f0
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-19 10:50:51 -0300

    feat(atividades): implement AtividadeService for activity management with CRUD operations

---
+++ src/app/services/atividades/atividade.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AtividadeRequest } from 'src/app/models/interfaces/atividades/AtividadeRequest';
+import { AtividadeResponse } from 'src/app/models/interfaces/atividades/AtividadeResponse';
+import { environment } from 'src/environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AtividadeService {
+  private API_URL = `${environment.API_URL}/atividades`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllAtividades(): Observable<Array<AtividadeResponse>> {
+    return this.http.get<Array<AtividadeResponse>>(this.API_URL);
+  }
+
+  createAtividade(data: AtividadeRequest): Observable<AtividadeResponse> {
+    return this.http.post<AtividadeResponse>(this.API_URL, data);
+  }
+
+  editAtividade(id: number, data: AtividadeRequest): Observable<AtividadeResponse> {
+    return this.http.put<AtividadeResponse>(`${this.API_URL}/${id}`, data);
+  }
+
+  deleteAtividade(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/${id}`);
+  }
+}
