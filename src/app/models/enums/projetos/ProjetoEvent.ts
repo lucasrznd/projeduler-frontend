b5d90272ad6c6d2f5294ca56ba9e1aabf776a96c
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-22 12:09:43 -0300

    feat(projetos): update user roles visibility and modify event names for clarity

--- src/app/models/enums/projetos/ProjetoEvent.ts
+++ src/app/models/enums/projetos/ProjetoEvent.ts
@@ -1,6 +1,6 @@
 export enum ProjetoEvent {
   ADD_PROJETO_EVENT = "Adicionar Projeto",
   ADD_USUARIO_PROJETO_EVENT = "Adicionar Usuários no Projeto",
-  EDIT_USUARIO_PROJETO_EVENT = "Editar Usuários do Projeto",
+  EDIT_USUARIO_PROJETO_EVENT = "Usuários no Projeto",
   EDIT_PROJETO_EVENT = "Editar Projeto"
 }
